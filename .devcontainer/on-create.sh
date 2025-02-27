2595f7a3b64d79a20fdf1bdfc052065cce928364
Cayo <k2@memyselfandi.com>
2025-02-27 15:26:52 +0100

    latest

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -1,10 +1,2 @@
-# create local registry
-docker network create k3d
-k3d registry create registry.localhost --port 5500
-docker network connect k3d k3d-registry.localhost
-
-# create k3d cluster
-k3d cluster create --registry-use k3d-registry.localhost:5500
-
-# install tilt to sync files to kubernetes
-curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | sudo bash
+docker build -t hashistackwebserver .
+docker run -d --name webserver -p 8080:80 hashistackwebserver
